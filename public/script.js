0ff71b56d37d64cb6817facb811d473c50742320
manishm7-tech <vaishnavmanish219@gmail.com>
2026-04-17 11:04:25 +0530

    Make Ejs Pages Responsive For Mobile And Tablet Users

--- public/script.js
+++ public/script.js
@@ -333,6 +333,29 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 300);
     })
 
+
+    let disconnect = document.querySelector(".disconnect")
+    let dissure = document.querySelector(".dissure")
+    let canceldisconnect = document.querySelector(".canceldisconnect")
+    disconnect.addEventListener("click",()=>{
+        dissure.classList.remove("hidden")
+        canceldisconnect.addEventListener("click",()=>{
+            dissure.classList.add("hidden")
+        })
+    })
+
+
+    let logoutbtn = document.querySelector(".logoutbtn")
+    let logoutsure = document.querySelector(".logoutsure")
+    let cancellogout = document.querySelector(".cancellogout")
+    logoutbtn.addEventListener("click",()=>{
+        logoutsure.classList.remove("hidden")
+        cancellogout.addEventListener("click",()=>{
+            logoutsure.classList.add("hidden")
+        })
+    })
+
+
 });
 
 async function likepost(userid) {
