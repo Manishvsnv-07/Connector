1fa4cfc838fcff2a01f974c8b4e80517faf6bda2
manishm7-tech <vaishnavmanish219@gmail.com>
2026-05-07 21:41:17 +0530

    use resend smtp

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,14 +2,13 @@ import dotenv, { populate } from "dotenv"
 dotenv.config();
 import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.resend.com",
     port: 465,
     secure: true,
     auth: {
-        user: process.env.EMAIL_NAME,
-        pass: process.env.EMAIL_PASS
-    },
-    family: 4
+        user: "resend",
+        pass: process.env.RESEND_API_KEY
+    }
 })
 
 export default transporter;
